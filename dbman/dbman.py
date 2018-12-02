3c60771952bbee2fdd2ee23a434a5eb1e406fb77
BugInMyHEAD <a@a>
2018-12-03 02:35:33 +0900

    remove trailing spaces

--- dbman/dbman.py
+++ dbman/dbman.py
@@ -12,17 +12,17 @@ class recipe_db_connection:
 # Open database connection
 db = pymysql.connect(
     host='localhost', port=3306, user='root', passwd='1111', db='sdadb', charset='utf8', autocommit=True)
- 
+
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
- 
+
 # execute SQL query using execute() method.
 cursor.execute("SELECT VERSION()")
- 
+
 # Fetch a single row using fetchone() method.
 data = cursor.fetchone()
- 
+
 print ("Database version : %s " % data)
- 
+
 # disconnect from server
 db.close()
